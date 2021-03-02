7bfb73d3f4cf05bf66958805f8c544285f4ba137
Kyle Scheuing <kyle@kylescheuing.com>
2021-03-02 06:31:09 -0500

    Add tests for CSV and JSON reporters

---
+++ reporter/csvreporter_test.py
@@ -0,0 +1,32 @@
+import unittest
+from .csvreporter import CSVReporter
+from .testhelper import create_test_log
+
+
+class CSVReporterTest(unittest.TestCase):
+    def testReport(self):
+        self.maxDiff = None
+        log = create_test_log()
+        reporter = CSVReporter(log)
+        report = reporter.generate_report()
+        lines = report.splitlines()
+        header, *entries = lines
+        self.assertEqual(header, "Timestamp,Event,Previous Money,New Money,Change")
+        self.assertSetEqual(
+            set(entries),
+            set(
+                [
+                    "2021-01-04T00:00:00,LOOT,50000000000,50000100000,100000",
+                    "2021-01-04T00:01:00,LOOT,50000100000,50000250125,150125",
+                    "2021-01-04T00:02:00,AUCTION_HOUSE_SELL,50000250125,51500250125,1500000000",
+                    "2021-01-04T00:03:00,AUCTION_HOUSE_COMMODITY_BUY,51500250125,49500250125,-2000000000",
+                    "2021-01-04T00:04:00,AUCTION_HOUSE_BID,49500250125,49000250125,-500000000",
+                    "2021-01-04T00:05:00,TRADE,49000250125,49000750125,500000",
+                    "2021-01-04T00:06:00,TRADE,49000750125,49000250125,-500000",
+                    "2021-01-04T00:07:00,GUILD_BANK_WITHDRAW,49000250125,59000250125,10000000000",
+                    "2021-01-04T00:08:00,GUILD_BANK_DEPOSIT,59000250125,54000250125,-5000000000",
+                    "2021-01-04T00:09:00,MAIL_IN,54000250125,54001005448,755323",
+                    "2021-01-04T00:10:00,MAIL_OUT,54001005448,54000505448,-500000",
+                ]
+            ),
+        )
---
+++ reporter/jsonreporter_test.py
@@ -0,0 +1,128 @@
+import unittest
+import json
+from .jsonreporter import JSONReporter
+from .testhelper import create_test_log
+
+
+class JSONReporterTest(unittest.TestCase):
+    def testReport(self):
+        self.maxDiff = None
+        log = create_test_log()
+        reporter = JSONReporter(log)
+        report = reporter.generate_report()
+        parsed_log = json.loads(report)
+        self.assertListEqual(
+            parsed_log,
+            [
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "LOOT",
+                    "prevMoney": 50000000000,
+                    "newMoney": 50000100000,
+                    "timestamp": "2021-01-04T00:00:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "LOOT",
+                    "prevMoney": 50000100000,
+                    "newMoney": 50000250125,
+                    "timestamp": "2021-01-04T00:01:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "AUCTION_HOUSE_SELL",
+                    "prevMoney": 50000250125,
+                    "newMoney": 51500250125,
+                    "timestamp": "2021-01-04T00:02:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "AUCTION_HOUSE_COMMODITY_BUY",
+                    "prevMoney": 51500250125,
+                    "newMoney": 49500250125,
+                    "timestamp": "2021-01-04T00:03:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "AUCTION_HOUSE_BID",
+                    "prevMoney": 49500250125,
+                    "newMoney": 49000250125,
+                    "timestamp": "2021-01-04T00:04:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "TRADE",
+                    "prevMoney": 49000250125,
+                    "newMoney": 49000750125,
+                    "timestamp": "2021-01-04T00:05:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "TRADE",
+                    "prevMoney": 49000750125,
+                    "newMoney": 49000250125,
+                    "timestamp": "2021-01-04T00:06:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "GUILD_BANK_WITHDRAW",
+                    "prevMoney": 49000250125,
+                    "newMoney": 59000250125,
+                    "timestamp": "2021-01-04T00:07:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "GUILD_BANK_DEPOSIT",
+                    "prevMoney": 59000250125,
+                    "newMoney": 54000250125,
+                    "timestamp": "2021-01-04T00:08:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "MAIL_IN",
+                    "prevMoney": 54000250125,
+                    "newMoney": 54001005448,
+                    "timestamp": "2021-01-04T00:09:00",
+                },
+                {
+                    "character": {
+                        "name": "Testname",
+                        "realm": "Testrealm",
+                    },
+                    "type": "MAIL_OUT",
+                    "prevMoney": 54001005448,
+                    "newMoney": 54000505448,
+                    "timestamp": "2021-01-04T00:10:00",
+                },
+            ],
+        )
